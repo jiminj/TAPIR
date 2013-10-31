715ea1391455294f12ec4a89ebf2ddc47b835040
Luke Kim <doiluvu@gmail.com>
2013-10-31 21:28:31 +0900

    AudioBufferCorrelationTest added

---
+++ AudioBufferCorrelationTest/AudioBufferCorrelationTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AudioBufferCorrelationTest
+//
+//  Created by dilu on 9/25/13.
+//  Copyright (c) 2013 dilu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
