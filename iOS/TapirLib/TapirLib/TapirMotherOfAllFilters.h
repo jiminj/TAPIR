7ea1f67e385b30f600f1d4addb946e89f97b2e46
Luke Kim <doiluvu@gmail.com>
2013-11-27 15:24:03 +0900

    general filter introduced

---
+++ iOS/TapirLib/TapirLib/TapirMotherOfAllFilters.h
@@ -0,0 +1,13 @@
+//
+//  TapirMotherOfAllFilters.h
+//  TapirLib
+//
+//  Created by dilu on 11/26/13.
+//  Copyright (c) 2013 Jimin Jeon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TapirMotherOfAllFilters : NSObject
+
+@end
---
+++ iOS/TapirLib/TapirLib/TapirMotherOfAllFilters.m
@@ -0,0 +1,13 @@
+//
+//  TapirMotherOfAllFilters.m
+//  TapirLib
+//
+//  Created by dilu on 11/26/13.
+//  Copyright (c) 2013 Jimin Jeon. All rights reserved.
+//
+
+#import "TapirMotherOfAllFilters.h"
+
+@implementation TapirMotherOfAllFilters
+
+@end
