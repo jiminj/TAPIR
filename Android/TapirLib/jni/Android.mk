5c25b85ece34066c7d49b0534d5929e68507a44c
Jimin Jeon <haven04@gmail.com>
2014-02-19 04:21:29 +0900

    Add a sample project for Android

--- Android/TapirLib/jni/Android.mk
+++ Android/TapirLib/jni/Android.mk
@@ -1,19 +1,21 @@
+
 ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
+	JNI_PATH := $(abspath $(call my-dir))
+	LOCAL_PATH := $(JNI_PATH)
 
-	
-	LOCAL_PATH := $(call my-dir)
-
-	LIB_PATH := ../../../libTapir
+	LIB_PATH := $(JNI_PATH)/../../../libTapir
 	SRC_PATH := $(LIB_PATH)/src
 	INCLUDE_PATH := $(LIB_PATH)/include
 	NDK_PATH := ~/Development/android/android-ndk-r9c
-	BUILD_PRODUCTS_DIR := ./obj/local/$(TARGET_ARCH_ABI)
-	
+	BUILD_PRODUCTS_DIR := $(JNI_PATH)/../obj/local/$(TARGET_ARCH_ABI)
+ 
 	include $(CLEAR_VARS)
+	SRC_PATH := $(LIB_PATH)/src
+	INCLUDE_PATH := $(LIB_PATH)/include
+	
 	LOCAL_C_INCLUDES := sources/cxx-stl/gnu-libstdc++/include/
 
 	LOCAL_MODULE    := tapir
-	LOCAL_INCLUDE_FILES := $(INCLUDE_PATH)
 	LOCAL_C_INCLUDES := $(NDK_PATH)/sources/cpufeatures
 	LOCAL_STATIC_LIBRARIES  :=  neon_utils cpufeatures
 	LOCAL_ARM_NEON  := true
@@ -48,16 +50,14 @@ ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
 
 	include $(CLEAR_VARS)
 
-
-endif
-#copy files
-
-COPY_DEST = ../libtapir-build
+	#copy files
+	COPY_DEST = $(JNI_PATH)/../../libtapir-build
 
 all : 
 	mkdir -p $(COPY_DEST)/lib $(COPY_DEST)/include/android
 	cp $(BUILT_RESULT) $(COPY_DEST)/lib/
-	cp ../../libTapir/include/*.h $(COPY_DEST)/include/
-	cp ../../libTapir/include/android/*.h $(COPY_DEST)/include/android
+	cp $(INCLUDE_PATH)/*.h $(COPY_DEST)/include/
+	cp $(INCLUDE_PATH)/android/*.h $(COPY_DEST)/include/android
 
+endif
 
