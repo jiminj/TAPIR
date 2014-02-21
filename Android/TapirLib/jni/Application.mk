6c3a4147d61af6b402cd5f5e2ca1cc2ac6479bec
Jimin Jeon <haven04@gmail.com>
2014-02-22 00:58:47 +0900

    Eliminated Android to update masterbranch

--- Android/TapirLib/jni/Application.mk
+++
@@ -1,9 +0,0 @@
-APP_ABI := armeabi-v7a
-APP_OPTIM := release
-APP_PLATFORM := android-14
-
-NDK_TOOLCHAIN_VERSION := clang
-LOCAL_CPPFLAGS += -std=gnu++11
-LOCAL_CPPFLAGS += -O2
-
-APP_STL := libc++_static
