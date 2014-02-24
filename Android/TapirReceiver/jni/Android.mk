626364029344323409f93524c18d2392ed5b3fe4
Jimin Jeon <haven04@gmail.com>
2014-02-25 01:40:09 +0900

    oops, accidently missed Makefiles for TapirReciever at last commit

--- Android/TapirReceiver/jni/Android.mk
+++ Android/TapirReceiver/jni/Android.mk
@@ -1,8 +1,40 @@
-LOCAL_PATH := $(call my-dir)
 
-include $(CLEAR_VARS)
+ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
+	
+	JNI_PATH := $(abspath $(call my-dir))
+	LOCAL_PATH := $(abspath $(call my-dir))
+	
+#load for NE10
+ 	include $(CLEAR_VARS)
+	NE10_LIB_PATH := $(JNI_PATH)/../../Ne10
+	LOCAL_MODULE := libNE10
+	
+	LOCAL_EXPORT_C_INCLUDES := $(NE10_LIB_PATH)/inc
+	LOCAL_SRC_FILES := $(NE10_LIB_PATH)/lib/libNE10.a
+	include $(PREBUILT_STATIC_LIBRARY)
+ 	
+#load for libTapir	
+	include $(CLEAR_VARS)
+	TAPIR_LIB_PATH = $(JNI_PATH)/../../libtapir-build
+	LOCAL_MODULE := libTapir
+	
+	LOCAL_EXPORT_C_INCLUDES := $(TAPIR_LIB_PATH)/include
+	LOCAL_SRC_FILES := $(TAPIR_LIB_PATH)/lib/libtapir.a
+	include $(PREBUILT_STATIC_LIBRARY)
+	
+	
+	include $(CLEAR_VARS)	
+	
+	LOCAL_STATIC_LIBRARIES := libTapir libNE10 neon_utils cpufeatures 
+	LOCAL_ARM_NEON  := true
+	
+	LOCAL_MODULE    := TapirReceiver
+	LOCAL_SRC_FILES := TapirReceiver.cpp
+	
+	LOCAL_C_INCLUDES += $(NDK_PATH)/sources/cpufeatures
+	LOCAL_ALLOW_UNDEFINED_SYMBOLS := true
+	LOCAL_LDLIBS := -llog
+	
+	include $(BUILD_SHARED_LIBRARY)
 
-LOCAL_MODULE    := TapirReceiver
-LOCAL_SRC_FILES := TapirReceiver.cpp
-
-include $(BUILD_SHARED_LIBRARY)
+endif
