4dadaa44544a65243b770d14461c25bf4fa96380
Jimin Jeon <haven04@gmail.com>
2016-03-13 23:15:03 +0900

    fix

--- iOS/BitlyUrlShortener/BitlyUrlShortener/LKBitlyUrlShortener.m
+++ iOS/BitlyUrlShortener/BitlyUrlShortener/LKBitlyUrlShortener.m
@@ -11,27 +11,31 @@
 
 @interface LKBitlyUrlShortener ()
 {
-    NSString * kBitlyAccessToken;
+    NSString * bitlyAccessToken;
 }
-@property NSString * kBitlyAccessToken;
+@property NSString * bitlyAccessToken;
 @end
 
 @implementation LKBitlyUrlShortener
 @synthesize delegate;
-@synthesize kBitlyAccessToken;
+@synthesize bitlyAccessToken;
 
 - (id)init
 {
     if(self = [super init])
     {
-        kBitlyAccessToken = @"***REMOVED***";
+        //You should write your own Key.plist file including your bitly access token.
+        //the format can be refered to Key_format.plist.
+        NSDictionary *dictionary = [NSDictionary dictionaryWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"Key" ofType:@"plist"]];
+        bitlyAccessToken = [dictionary objectForKey:@"Key"];
+//
     }
     return self;
 }
 
 -(void)shortenUrl:(NSString *)original{
     AFHTTPSessionManager * sessionManager = [AFHTTPSessionManager manager] ;
-    [sessionManager GET:[NSString stringWithFormat:@"https://api-ssl.bitly.com/v3/shorten?access_token=%@&longUrl=%@", [self kBitlyAccessToken], original]
+    [sessionManager GET:[NSString stringWithFormat:@"https://api-ssl.bitly.com/v3/shorten?access_token=%@&longUrl=%@", [self bitlyAccessToken], original]
              parameters:nil
                progress:nil
                 success:^(NSURLSessionTask *task, id responseObject) {
@@ -47,7 +51,7 @@
 
 -(void)getOriginalUrl:(NSString *)original{
     AFHTTPSessionManager* sessionManager = [[AFHTTPSessionManager alloc] init];
-    [sessionManager GET:[NSString stringWithFormat:@"https://api-ssl.bitly.com/v3/expand?access_token=%@&shortUrl=%@", [self kBitlyAccessToken], original]
+    [sessionManager GET:[NSString stringWithFormat:@"https://api-ssl.bitly.com/v3/expand?access_token=%@&shortUrl=%@", [self bitlyAccessToken], original]
              parameters:nil
                progress:nil
                 success:^(NSURLSessionTask *task, id responseObject) {
